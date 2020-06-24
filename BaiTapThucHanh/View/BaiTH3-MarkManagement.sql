b6f25bdcf8848e037f5e2470600bb1e5732d6e0a
Pilly'ss <zadajtjeu@gmail.com>
2020-06-24 11:57:43 +0700

    moved

---
+++ BaiTapThucHanh/View/BaiTH3-MarkManagement.sql
@@ -0,0 +1,139 @@
+﻿use master
+go
+if(exists(select * from sysdatabases where name='MarkManagement'))
+	drop database MarkManagement
+go
+
+/* A. Create Database */
+
+create database MarkManagement
+
+go
+use MarkManagement
+go
+/* B. Create Table */
+
+create table Students(
+	StudentID nvarchar(12) not null primary key,
+	StudentName nvarchar(25) not null,
+	DateofBirth datetime not null,
+	Email nvarchar(40),
+	Phone nvarchar(12),
+	Class nvarchar(10)
+)
+
+create table Subjects(
+	SubjectID nvarchar(10) not null primary key,
+	SubjectName nvarchar(25) not null
+)
+
+create table Mark(
+	ID int not null identity,
+	StudentID nvarchar(12) not null,
+	SubjectID nvarchar(10) not null,
+	Theory tinyint,
+	Practical tinyint,
+	Date datetime,
+	constraint Pri_Key primary key(ID, StudentID, SubjectID),
+	constraint Mark_Stu foreign key(StudentID) references Students(StudentID),
+	constraint Mark_Sub foreign key(SubjectID) references Subjects(SubjectID)
+)
+
+go
+
+
+/* C. Insert Data */
+
+insert into Students 
+values('AV0807005',N'Mail Trung Hiếu','11/10/1989','trunghieu@yahoo.com','0904115116','AV1'),
+		('AV0807006',N'Nguyễn Quý Hùng','2/12/1988','quyhung@yahoo.com','0955667787','AV2'),
+		('AV0807007',N'Đỗ Đắc Huỳnh','2/1/1990','dachuynh@yahoo.com','0988574747','AV2'),
+		('AV0807009',N'An Đăng Khê','6/3/1986','dangkhue@yahoo.com','0986757463','AV1'),
+		('AV0807010',N'Nguyễn T. Tuyết Lan','12/7/1989','tuyetlan@gmail.com','0983310342','AV2'),
+		('AV0807011',N'Đinh Phụng Long','2/12/1990','phunglong@yahoo.com','','AV1'),
+		('AV0807012',N'Nguyễn Tuấn Nam','2/3/1990','tuannam@yahoo.com','','AV1')
+
+insert into Subjects
+values('S001','SQL'), ('S002','Java Simplefield'), ('S003', 'Active Server Page')
+
+insert into Mark
+values('AV0807005','S001',8,25,'6/5/2008'),
+ ('AV0807006','S002',16,30,'6/5/2008'),
+ ('AV0807007','S001',10,25,'6/5/2008'),
+ ('AV0807009','S003',7,13,'6/5/2008'),
+ ('AV0807010','S003',9,16,'6/5/2008'),
+ ('AV0807011','S002',8,30,'6/5/2008'),
+ ('AV0807012','S001',7,31,'6/5/2008'),
+ ('AV0807005','S002',12,11,'6/6/2008'),
+ ('AV0807009','S003',11,20,'6/6/2008'),
+ ('AV0807010','S001',7,6,'6/6/2008')
+
+ go
+
+ -- test
+ select * from Students
+ select * from Subjects
+ select * from Mark
+ go
+ /* D. Execute Query */
+
+
+
+ -- 1. Show Student
+ select * from Students
+ go
+
+ -- 2. Show student at AV1
+ select * from Students where Class='AV1'
+ go
+
+ -- 3. Sử dụng lệnh UPDATE để chuyển sinh viên có mã AV0807012 sang lớp AV2
+ update Students set Class='AV2' where StudentID='AV0807012'
+ go
+ --test
+select * from Students
+go
+
+-- 4. Tính tổng số sinh viên của từng lớp
+select Class, COUNT(*) as 'Total Student' from Students group by Class
+go
+
+-- 5. Hiển thị danh sách sinh viên lớp AV2 được sắp xếp tăng dần theo StudentName
+select * from Students where Class='AV2' order by StudentName ASC
+go
+
+-- 6. Hiển thị danh sách sinh viên không đạt lý thuyết môn S001 (theory <10) thi ngày 6/5/2008
+select * from Students where StudentID in (select StudentID from Mark where SubjectID='S001' and Theory<10 and Date='6/5/2008')
+
+-- Cach 2
+select Students.StudentID, Students.StudentName, Mark.SubjectID, Mark.Theory, Mark.Date
+from Students inner join Mark on Students.StudentID = Mark.StudentID
+where SubjectID='S001' and Theory<10 and Date='6/5/2008'
+
+go
+
+-- 7. Hiển thị tổng số sinh viên không đạt lý thuyết môn S001. (theory <10)
+select COUNT(*) as 'Students faile S001' from Mark where SubjectID='S001' and Theory<10
+go
+
+-- 8. Hiển thị Danh sách sinh viên học lớp AV1 và sinh sau ngày 1/1/1980
+select * from Students where Class='AV1' and DateofBirth>'1/1/1980'
+go
+
+-- 9. Xoá sinh viên có mã AV0807011
+delete from Mark where StudentID='AV0807011'
+delete from Students where StudentID='AV0807011'
+go
+--test
+select * from Students
+select * from Mark
+go
+
+-- 10. Hiển thị danh sách sinh viên dự thi môn có mã S001 ngày 6/5/2008
+--bao gồm các trường sau: StudentID, StudentName, SubjectName, Theory, Practical, Date
+select Students.StudentID, StudentName, SubjectName, Theory, Practical, Date
+from Students inner join Mark on Students.StudentID = Mark.StudentID
+			  inner join Subjects on Mark.SubjectID = Subjects.SubjectID
+where Mark.SubjectID = 'S001' and Mark.Date = '6/5/2008'
+go
+
---
+++ BaiTapThucHanh/View/BaiTH4-QLKHO.sql
@@ -0,0 +1,196 @@
+﻿use master
+go
+if(exists(select * from sysdatabases where name='QLKHO'))
+	drop database QLKHO
+go
+
+/* 1. create database
+	Nhap (SoHDN,MaVT,SoLuongN,DonGiaN,NgayN)
+	Xuat (SoHDX,MaVT,SoLuongX,DonGiaX,NgayX)
+	Ton (MaVT,TenVT,SoLuongT)
+Xây dựng mô hình quan hệ cho 3 bảng trên,
+	 ở bảng nhập hãy nhập 3 phiếu nhập, 
+	 ở  bảng xuất nhập 2 phiếu xuất, 
+	 bảng Ton  nhập 5 vật tư khác nhau.
+ */
+
+use master
+go
+create database QLKHO
+go
+
+-- create table
+use QLKHO
+go
+
+create table Ton(
+	MaVT nvarchar(10) not null primary key,
+	TenVT Nvarchar(30) not null,
+	SoLuongT int default 0
+)
+
+create table Nhap(
+	SoHDN nvarchar(10) not null,
+	MaVT nvarchar(10) not null,
+	SoLuongN int not null,
+	DonGiaN money default 0,
+	NgayN DateTime,
+	constraint Nhap_Ton foreign key(MaVT) references Ton(MaVT),
+	constraint Nhap_Pri_Key Primary Key(SoHDN, MaVT)
+)
+
+create table Xuat(
+	SoHDX nvarchar(10) not null,
+	MaVT nvarchar(10) not null,
+	SoLuongX int not null,
+	DonGiaX money default 0,
+	NgayX DateTime,
+	constraint Xuat_Ton foreign key(MaVT) references Ton(MaVT),
+	constraint Xuat_Pri_Key Primary Key(SoHDX, MaVT)
+)
+
+go
+
+
+-- insert data
+insert into Ton
+values ('VT0001',N'Ống đồng',100),
+		('VT0002',N'Ống thép D6',0),
+		('VT0003',N'Ống thép D8',200),
+		('VT0004',N'Thép lá',50),
+		('VT0005',N'Thép U',90)
+go
+
+-- insert Nhap-Xuat
+insert into Nhap
+values('HDN0001','VT0001', 380, 50000, '1/20/2015'),
+		('HDN0001','VT0004', 100, 18000, '1/20/2015'),
+		('HDN0002','VT0005', 150, 30000, '2/1/2015'),
+		('HDN0003','VT0003', 500, 85000, '3/22/2016'),
+		('HDN0003','VT0001', 200, 52000, '3/22/2016')
+insert into Xuat
+values('HDX0001','VT0001', 10, 55000, '4/3/2015'),
+		('HDX0001','VT0005', 52, 22000, '4/3/2015'),
+		('HDX0002','VT0001', 50, 56000, '5/12/2020'),
+		('HDX0002','VT0003', 150, 35000, '5/12/2020')
+go
+-- test
+select * from Nhap
+select * from Xuat
+select * from Ton
+go
+
+
+
+/* 2. thống kê tiền bán theo mã vật tư gồm 
+MaVT, TênVT, TienBan (TienBan=SoLuongX*DonGiaX) */
+create view TienBanVatTu as
+select Ton.MaVT, TenVT, SUM(SoLuongX*DonGiaX) as N'Tiền bán'
+from Ton inner join Xuat on Ton.MaVT=Xuat.MaVT
+group by Ton.MaVT, TenVT
+go
+--test
+select * from TienBanVatTu
+go
+
+/* 3. thống kê soluongxuat theo tên vattu */
+create view VatTuDaXuat as
+select TenVT, SUM(SoLuongX) as N'SL Xuất'
+from Ton inner join Xuat on Ton.MaVT=Xuat.MaVT
+group by TenVT
+go
+--test
+select * from VatTuDaXuat
+go
+
+/* 4. thống kê soluongnhap theo tên vật tư */
+create view VatTuDaNhap as
+select TenVT, SUM(SoLuongN) as N'SL Nhập'
+from Ton inner join Nhap on Ton.MaVT=Nhap.MaVT
+group by TenVT
+go
+--test
+select * from VatTuDaNhap
+go
+
+/* 5. đưa ra tổng soluong còn trong kho 
+biết còn = nhap – xuất + tồn theo từng nhóm vật tư */
+create view SLVatTuCon as
+select Ton.MaVT, TenVT, SUM(SoLuongN)-SUM(SoLuongX)+SUM(SoLuongT) as 'SoLuongCon'
+from Ton inner join Nhap on Ton.MaVT=Nhap.MaVT
+		 inner join Xuat on Ton.MaVT=Xuat.MaVT
+group by Ton.MaVT, TenVT
+go
+--test
+select * from SLVatTuCon
+go
+
+
+/* 6. đưa ra tên vật tư  số lượng tồn nhiều nhất */
+create view SLTonMax as
+select TenVT from Ton 
+where SoLuongT = (select MAX(SoLuongT) from Ton)
+go
+-- test
+select * from SLTonMax
+go
+
+
+
+/* 7. đưa ra các vật tư có tổng số lượng xuất lớn hơn 100 */
+create view SLXuat100 as
+select Ton.MaVT,TenVT
+from Ton inner join Xuat on Ton.MaVT=Xuat.MaVT
+group by Ton.MaVT,TenVT having SUM(SoLuongX) > 100
+
+go
+--test
+select * from SLXuat100
+go
+
+
+
+/* 8. Tạo view đưa ra tháng xuất,
+năm xuất, tổng số lượng xuất 
+thống kê theo tháng và năm xuất */
+create view NgayXuat as
+select MONTH(NgayX) as N'Tháng', YEAR(NgayX) as N'Năm', SUM(SoLuongX) as N'Tổng số lượng xuất'
+from Xuat
+group by MONTH(NgayX), YEAR(NgayX)
+
+go
+--test
+select * from NgayXuat
+go
+
+
+
+/* 9. tạo view đưa ra mã vật tư. tên vật tư. 
+số lượng nhập. số lượng xuất. đơn giá N. 
+đơn giá X. ngày nhập. Ngày xuất */
+create view ShowNhapXuat as
+select Ton.MaVT, TenVT, SoLuongN, SoLuongX, DonGiaN, DonGiaX, NgayN, NgayX
+from Ton inner join Nhap on Ton.MaVT=Nhap.MaVT
+		 inner join Xuat on Ton.MaVT=Xuat.MaVT
+
+go
+--test
+select * from ShowNhapXuat
+go
+
+
+/* 10. Tạo view đưa ra mã vật tư. 
+tên vật tư và tổng số lượng còn lại trong kho. 
+biết còn lại = SoluongN-SoLuongX+SoLuongT
+theo từng loại Vật tư  trong năm 2015 */
+
+create view SLVatTuCon2015 as
+select Ton.MaVT, TenVT, SUM(SoLuongN)-SUM(SoLuongX)+SUM(SoLuongT) as 'SoLuongCon'
+from Ton inner join Nhap on Ton.MaVT=Nhap.MaVT
+		 inner join Xuat on Ton.MaVT=Xuat.MaVT
+where YEAR(NgayN)=2015 and YEAR(NgayX)=2015
+group by Ton.MaVT, TenVT
+go
+--test
+select * from SLVatTuCon2015
+go
